a7cf531eef8417a990c3ead3fe27c73a43140575
tasha72 <48554832+tasha72@users.noreply.github.com>
2021-09-17 23:40:25 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
 basic.forever(function () {
-	
+    led.plot(randint(0, 5), randint(0, 5))
+    basic.pause(100)
+    if (input.isGesture(Gesture.Shake)) {
+        led.toggle(randint(0, 5), randint(0, 5))
+    }
 })
